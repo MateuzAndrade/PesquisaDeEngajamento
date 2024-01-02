a9713a3195b088ec8817e708584f9feea2f3d43c
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2024-01-02 06:49:58 -0300

    Tela de Login

---
+++ PesquisaDeEngajamentoFront/PesquisaDeEngajamento/src/App.tsx
@@ -0,0 +1,38 @@
+import './style.css'
+import logo from './assets/Pegmatita-removebg-preview.png'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="container-login">
+          <div className="wrapper-login">
+            <form className="login-form">
+              <span className="login-form-title">
+                Pesquisa de Engajamento
+              </span>
+              <span className="login-form-title">
+                 <img src={logo} alt="" />
+              </span>
+              <div className="wrap-input">
+                <input type="email" />
+                <span className="focus-input" data-placeholder="Email" ></span>
+              </div>
+              <div className="wrap-input">
+                <input type="password" />
+                <span className="focus-input" data-placeholder="Password"></span>
+              </div>
+              <div className="container-login-form-btn">
+                <button className="login-form-btn"></button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+
+}
+export default App
